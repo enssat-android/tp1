ae431e99b0a2396e9ddc7ff1e01e92b2dc8cff5e
Guillaume Châtelet <guillaume.chatelet@orange.com>
2018-10-03 14:23:33 +0200

    step 7 work

--- app/src/main/java/fr/enssat/firstapp/MainActivity.java
+++ app/src/main/java/fr/enssat/firstapp/MainActivity.java
@@ -2,6 +2,9 @@ package fr.enssat.firstapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Show a toast
+     * @param view -- the view that is clicked
+     */
+    public void toastMe(View view){
+        // Toast myToast = Toast.makeText(this, message, duration);
+        Toast myToast = Toast.makeText(this, "Hello Toast!",
+                Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+
+    public void countMe (View view) {
+        // Get the text view.
+        TextView showCountTextView = (TextView)
+                findViewById(R.id.textView);
+
+        // Get the value of the text view.
+        String countString = showCountTextView.getText().toString();
+
+        // Convert value to a number and increment it.
+        Integer count = Integer.parseInt(countString);
+        count++;
+
+        // Display the new value in the text view.
+        showCountTextView.setText(count.toString());
     }
 }
