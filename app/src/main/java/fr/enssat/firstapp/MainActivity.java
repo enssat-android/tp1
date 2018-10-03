b457290a70cf94d5a594755d581090b7fac1d965
Guillaume Châtelet <guillaume.chatelet@orange.com>
2018-10-03 14:32:27 +0200

    step 8 work

--- app/src/main/java/fr/enssat/firstapp/MainActivity.java
+++ app/src/main/java/fr/enssat/firstapp/MainActivity.java
@@ -1,12 +1,15 @@
 package fr.enssat.firstapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private static final String TOTAL_COUNT = "total_count";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +28,7 @@ public class MainActivity extends Activity {
         myToast.show();
     }
 
-    public void countMe (View view) {
+    public void countMe(View view) {
         // Get the text view.
         TextView showCountTextView = (TextView)
                 findViewById(R.id.textView);
@@ -39,5 +42,25 @@ public class MainActivity extends Activity {
 
         // Display the new value in the text view.
         showCountTextView.setText(count.toString());
+    }
+
+    public void randomMe(View view) {
+        // Create an Intent to start the second activity
+        Intent randomIntent = new Intent(this, SecondActivity.class);
+
+        // Get the text view that shows the count.
+        TextView showCountTextView = (TextView) findViewById(R.id.textView);
+
+        // Get the value of the text view.
+        String countString = showCountTextView.getText().toString();
+
+        // Convert the count to an int
+        int count = Integer.parseInt(countString);
+
+        // Add the count to the extras for the Intent.
+        randomIntent.putExtra(TOTAL_COUNT, count);
+
+        // Start the new activity.
+        startActivity(randomIntent);
     }
 }
